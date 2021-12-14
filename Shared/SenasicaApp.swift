788702126f4817c439b4003749800a17b5ade5d1
kyusol <kyus.soto@gmail.com>
2021-12-13 19:32:01 -0600

    Initial Commit

---
+++ Shared/SenasicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenasicaApp.swift
+//  Shared
+//
+//  Created by Gerardo Uriel Soto Miranda on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SenasicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
